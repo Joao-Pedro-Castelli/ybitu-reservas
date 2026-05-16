3cc1c905f8ba16d58b5cfb564ff363bbedb5e955
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-05-16 11:38:01 -0300

    Move some css styles to Feedback.css

--- new-repo/Ybitu-Reservas/src/pages/Feedback.tsx
+++ new-repo/Ybitu-Reservas/src/pages/Feedback.tsx
@@ -7,7 +7,7 @@ export default function Feedback() {
         <>
 
             <div className="container mx-auto">
-                <div className="mt-12 lg:mt-16 text-5xl flex items-center justify-center">Feedback</div>
+                <div className="titulo">Feedback</div>
                 <div className="mt-6 lg:mt-12 lg:flex">
                     <div className="p-8 lg:max-w-1/2">
                         <p className="xl:text-lg">Conte para nós como foi passar esse tempo conosco! Seu comentário é muito importante para que possamos
@@ -15,29 +15,29 @@ export default function Feedback() {
                         </p>
                         <p className="mt-4 xl:text-lg">Agradecemos pela confiança durante sua estadia, volte sempre!!</p>
                     </div>
-                    <div className="bg-[#D9D9D9] p-8 rounded-md">
+                    <div className="grid_area">
                         <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                            <div className="flex flex-col">
+                            <div className="grid_element">
                                 <label htmlFor="">Nome:</label>
-                                <input type="text" id="" className="bg-[#F5F0E6] py-3 px-3 border rounded" placeholder="Insira seu nome" />
+                                <input type="text" id="" className="input_text" placeholder="Insira seu nome" />
                             </div>
 
-                            <div className="flex flex-col">
+                            <div className="grid_element">
                                 <label htmlFor="email">E-mail:</label>
-                                <input type="email" id="email" className="bg-[#F5F0E6] py-3 px-3 border rounded" placeholder="Insira seu e-mail" />
+                                <input type="email" id="email" className="input_text" placeholder="Insira seu e-mail" />
                             </div>
 
-                            <div className="flex flex-col">
+                            <div className="grid_element">
                                 <label htmlFor="comentario">Comentário:</label>
-                                <textarea className="resize-y py-3 px-3 rounded bg-[#F5F0E6] min-h-[150px] xl:min-h-[200px] 2xl:min-h-[250px]" name="" id="comentario" placeholder="Deixe aqui sua mensagem..."></textarea>
+                                <textarea className="area_style" name="" id="comentario" placeholder="Deixe aqui sua mensagem..."></textarea>
                             </div>
-                            <div className="flex flex-col">
+                            <div className="grid_element">
                                 <label htmlFor="comentario">Adicione arquivos:</label>
-                                <input className="border bg-gray-200 rounded file:border file:border-black file:bg-white file:font-bold file:py-1 file:px-2 file:rounded" type="file" accept="image/*" multiple />
+                                <input className="image_button" type="file" accept="image/*" multiple />
                             </div>
 
 
-                            <button className="bg-[#1F6F50] text-white p-3 rounded md:col-span-2">Enviar</button>
+                            <button className="btn_style">Enviar</button>
                         </form>
                     </div>
                 </div>
